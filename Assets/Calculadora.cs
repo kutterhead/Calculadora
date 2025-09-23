ec7e9710cb70f8d45415a389446ff81d5fee412a
Kutterhead <esseanx@gmail.com>
2025-09-23 21:32:50 +0200

    Terminado  ejercicio. Solucionado problema de pulsar Igual.

--- Assets/Calculadora.cs
+++ Assets/Calculadora.cs
@@ -13,6 +13,7 @@ public class Calculadora : MonoBehaviour
 
     int actualOperation = 0;//número que indica el tipo de operación
 
+    [SerializeField]
     int lastOperation = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -38,20 +39,48 @@ public class Calculadora : MonoBehaviour
             }
         }else
         {
+            if (igualPulsado)
+            {
+                igualPulsado = false;
+            }
+            if (lastOperation==0)
+            {
+
+                textoResultado.text = "";
+
+            }
+
             textoResultado.text += numero;
 
         }
 
     }
 
+    bool igualPulsado = false;
     public void opera(int operation)
     {
-    switch (lastOperation)
+        if (operation==5 && lastOperation == 5)
+        {
+            return;
+        }
+        
+
+
+        switch (lastOperation)
         {
                 case 0://no hay operación
 
-                acumulador = float.Parse(textoResultado.text);
-                textoResultado.text = "0";
+                if (igualPulsado)
+                {
+
+                }
+                else
+                {
+                    acumulador = float.Parse(textoResultado.text);
+                    textoResultado.text = "0";
+
+                }
+
 
                 break;
                 case 1://suma +
@@ -60,18 +89,58 @@ public class Calculadora : MonoBehaviour
                 break;
 
                 case 2://resta -
+                acumulador -= float.Parse(textoResultado.text);
+                textoResultado.text = acumulador.ToString();
                 break;
                 case 3://multiplica *
+                acumulador *= float.Parse(textoResultado.text);
+                textoResultado.text = acumulador.ToString();
                 break;
                 case 4:// divide /
+                acumulador /= float.Parse(textoResultado.text);
+                textoResultado.text = acumulador.ToString();
                 break;
-                default:
+                
+
+
+                default://solucion guarda el acumulador
+
+                acumulador = float.Parse(textoResultado.text);
+                textoResultado.text = "0";
+
                 break;
 
 
 
         }
-        lastOperation = operation;
+        if (operation == 5)
+        {
+            
+            //lastOperation = 0;
+            igualPulsado = true;
+        }
+        else
+        {
+            igualPulsado = false;
+
+        }
+            lastOperation = operation;
+    }
+
+
+    public void clearAll()
+    {
+        textoResultado.text = "0";
+        acumulador = 0f;
+        lastOperation = 0;
+
+    }
+
+    public void clearActual()
+    {
+        textoResultado.text = "0";
+
+
     }
 
 
